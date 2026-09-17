393ee04b2b72627478babd14468dc3f95adcef48
jacintolivis5-jc <jacintolivis5@gmail.com>
2026-09-17 20:46:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor do WhatsApp Bot funcionando!");
+});
+
+app.get("/webhook", (req, res) => {
+  res.send("Webhook funcionando!");
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Mensagem recebida:", req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando na porta ${PORT}`);
+});
